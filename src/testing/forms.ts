8a38d046deb7dec3d903f0d8ab548727b8b3e360
andymedinadev <107598069+andymedinadev@users.noreply.github.com>
2025-04-12 16:26:19 -0300

    feat: add user mock generation functions and checkbox setter to test forms

--- src/testing/forms.ts
+++ src/testing/forms.ts
@@ -26,3 +26,27 @@ export function setInputValue<T>(
   inputElement.dispatchEvent(new Event('input'));
   inputElement.dispatchEvent(new Event('blur'));
 }
+
+export function setCheckboxValue<T>(
+  fixture: ComponentFixture<T>,
+  selector: string,
+  value: boolean,
+  useTestId: boolean = false
+) {
+  let inputDebug: DebugElement;
+
+  try {
+    inputDebug = useTestId
+      ? queryById(fixture, selector)
+      : query(fixture, selector);
+  } catch (error) {
+    console.error('original error:', error);
+    throw new Error(`Input not found using selector: "${selector}"`);
+  }
+
+  const inputElement: HTMLInputElement = inputDebug.nativeElement;
+
+  inputElement.checked = value;
+  inputElement.dispatchEvent(new Event('change'));
+  inputElement.dispatchEvent(new Event('blur'));
+}
